2606acdda5be9e71f33cb444d9529c6de57a31c3
AlexBetita <alexbheb25@gmail.com>
2023-03-15 07:58:34 -0700

    added error handling for non existing urls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+import ErrorPage from './components/ErrorPage';
 import MainMenu from "./components/MainMenu";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:  <MainMenu/>,
+    errorElement: <ErrorPage />
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <MainMenu />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/ErrorPage/index.js
@@ -0,0 +1,34 @@
+import { useRouteError, Link } from "react-router-dom";
+
+import './index.css'
+
+import shock from '../../assets/images/shock.svg'
+import cfgLogo from '../../assets/images/logo.svg'
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <div style={{'height' : '80px', 'display': 'flex', 
+                   'alignItems' :'center'}}>
+        
+        <Link to={'/'}>
+            <img className='logo' src={cfgLogo}/>
+        </Link>
+
+      </div>
+      
+      <h1>Oops!</h1>
+      <img src={shock}/>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p className="error-status">
+        <i>{error.statusText || error.message}</i>
+        
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
