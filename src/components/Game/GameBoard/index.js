3a5afcaa86c548a6add21b04413b44207e625169
AlexBetita <alexbheb25@gmail.com>
2023-03-19 17:35:27 -0700

    used grid to create the board layout

--- src/components/Game/GameBoard/index.js
+++ src/components/Game/GameBoard/index.js
@@ -2,6 +2,8 @@ import './index.css'
 import boardBlack from '../../../assets/images/board-layer-black-large.svg'
 import boardWhite from '../../../assets/images/board-layer-white-large.svg'
 import cfgLogo from '../../../assets/images/logo.svg'
+import counterRed from '../../../assets/images/counter-red-large.svg'
+import counterYellow from '../../../assets/images/counter-yellow-large.svg'
 
 const GameBoard = ({timer, turnBackground, currentPlayer}) => {
 
@@ -23,9 +25,34 @@ const GameBoard = ({timer, turnBackground, currentPlayer}) => {
 
                 <div className='game-board'>
 
-                    <img className='game-board-white' src={boardWhite}/>
+                    <div style={{'position' : 'relative'}}>
+                        <div className='cells'>
+                            {
+                                Array(6).fill(null).map((e, index)=>{
+                                    const currentRow = index + 1
+                                    return <>
+                                            {
+                                                Array(7).fill(null).map((e, index)=>{
+                                                    const currentColumn = index + 1
+                                                    return <>
+                                                        <div className={`row-${currentRow} col-${currentColumn}`}>
+                                                            <img src={counterRed}/>
+                                                        </div>
+                                                    </>
+                                                })
+                                            }
+                                    </>
+                                })
+                            }
+                        </div>
+                        <img className='game-board-white' src={boardWhite}/>
+                    </div>
+
+
 
                     <img className='game-board-black' src={boardBlack}/>
+
+
                     <div className='turn-background'>
                         <img style={{'position' : 'absolute'}}src={turnBackground}/>
                         <div className='current-player'>
